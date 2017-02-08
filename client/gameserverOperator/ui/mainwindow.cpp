687fdf77ff6630788fb3ca01cff672aaae0b2148
face2wind <dream.liuguobiao@gmail.com>
2017-02-08 20:19:57 +0800

    client : commit operate request

--- client/gameserverOperator/ui/mainwindow.cpp
+++ client/gameserverOperator/ui/mainwindow.cpp
@@ -27,9 +27,20 @@ void MainWindow::SetOperateData(Protocol::SCGORequestCommandListACK *ack)
     for (Protocol::SCGOCommandOperateItem item : ack->operate_list)
         opera_str_list<<item.operate_name.c_str();
     ui->operate_type->addItems(opera_str_list);
+
+    project_list_ = ack->project_list;
+    operate_list_ = ack->operate_list;
 }
 
 void MainWindow::on_execute_btn_clicked()
 {
+    int project_index = ui->project_name->currentIndex();
+    int operate_index = ui->operate_type->currentIndex();
+    if (project_index >= project_list_.size() || operate_index >= operate_list_.size())
+        return;
 
+    Protocol::CSGORequestCommand cmd;
+    cmd.project_type = project_list_[project_index].project_type;
+    cmd.operate_type = operate_list_[operate_index].operate_type;
+    NetworkAgent::GetInstance().SendToServer(cmd);
 }
